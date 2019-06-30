ef99f3ca8615313dd184dae67efd303b0ba4ee61
JC Spark <sparkdoaz@gmail.com>
2019-06-30 10:01:31 +0800

    homework3
    upload the homework3

--- week3/Homework3/Homework3/Model.swift
+++ week3/Homework3/Homework3/Model.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+enum CheckInputHaveError: Error {
+    case accountIsEmpty
+    case passwordIsEmpty
+    case checkPasswordIsEmpty
+}
+enum CheckLoginSignHaveError: Error {
+    case loginFail
+    case signUpFail
+    case notAllowSign
+}
+
+func checkInput(account: String, password: String, checkPassword: String, logOrSign status: Int) throws {
+    if account == "" {
+        throw CheckInputHaveError.accountIsEmpty
+    } else if password == "" {
+        throw CheckInputHaveError.passwordIsEmpty
+    } else if  checkPassword == "" && status == 1 {
+        throw CheckInputHaveError.checkPasswordIsEmpty
+    }
+}
+
+func checkLogin(account: String, password: String) throws -> Bool{
+    if account == "appwork_school@gmail.com" && password == "1234" {
+        return true
+    } else {
+        throw CheckLoginSignHaveError.loginFail
+    }
+}
+
+func checkSignUp(account: String, password: String, checkPassword: String) throws -> Bool {
+    if checkPassword == password {
+        return true
+    } else if checkPassword != password {
+        throw CheckLoginSignHaveError.signUpFail
+    } else {
+        throw CheckLoginSignHaveError.notAllowSign
+    }
+}
+
+func updateTextFiled () {
+    // FIXME: clean the word in textfiled
+}
+
--- week3/Homework3/Homework3/ViewController.swift
+++ week3/Homework3/Homework3/ViewController.swift
@@ -8,15 +8,7 @@
 
 import UIKit
 
-enum CheckInputHaveError: Error {
-    case accountIsEmpty
-    case passwordIsEmpty
-    case checkPasswordIsEmpty
-}
-enum CheckLoginHaveError: Error {
-    case loginFail
-    case s
-}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var checkTexTLabel: UITextField!
@@ -35,6 +27,9 @@ class ViewController: UIViewController {
         checkLabel.textColor = UIColor.gray
         checkTexTLabel.isUserInteractionEnabled = false
         checkTexTLabel.backgroundColor = UIColor.gray
+        
+        
+        updateTextFiledWith()
     }
 
     @IBAction func indexSwitch(_ sender: UISegmentedControl) {
@@ -50,19 +45,76 @@ class ViewController: UIViewController {
     }
 
     @IBAction func loginButton() {
-        do {
-            guard let account = accountTextLabel.text, let password = passwordTextLabel.text, let checkPassowrd = checkLabel.text  else {
-                return
+        if let account = accountTextLabel.text, let password = passwordTextLabel.text, let checkPassowrd = checkLabel.text {
+            do {
+                try checkInput(account: account, password: password, checkPassword: checkPassowrd, logOrSign: indexLabel.selectedSegmentIndex)
+            } catch CheckInputHaveError.accountIsEmpty {
+                showPopAlert(title: "Error", message: "Account should not be empty")
+            } catch CheckInputHaveError.passwordIsEmpty {
+                showPopAlert(title: "Error", message: "Password should not be empty")
+            } catch CheckInputHaveError.checkPasswordIsEmpty {
+                showPopAlert(title: "Error", message: "CheckPassword should not be empty")
+            } catch let error {
+                fatalError("\(error)")
             }
-             try checkUserInput(account: account, password: password, checkPassword: checkPassword, status: layout.stateSegment.selectedSegmentIndex)
-            
-            
-            
-            
-            
+        }
+        if indexLabel.selectedSegmentIndex == 0 {
+            do {
+                guard let account = accountTextLabel.text, let password = passwordTextLabel.text else {
+                    return
+                }
+                if try checkLogin(account: account, password: password) {
+                    showPopAlert(title: "Login", message: "Welcome !")
+                }
+            } catch CheckLoginSignHaveError.loginFail {
+                showPopAlert(title: "Error", message: "login Fail")
+            } catch let error {
+                fatalError("\(error)")
+            }
+        } else if indexLabel.selectedSegmentIndex == 1 {
+            do {
+                guard let account = accountTextLabel.text, let password = passwordTextLabel.text, let checkPassword = checkTexTLabel.text else {
+                    return
+                }
+                if try checkSignUp(account: account, password: password, checkPassword: checkPassword) {
+                    showPopAlert(title: "SignUp", message: "Welcome !")
+                }
+            } catch CheckLoginSignHaveError.signUpFail {
+                showPopAlert(title: "Error", message: "Signup Fail")
+            } catch let error {
+                fatalError("\(error)")
+            }
+        }
+        updateTextFiledWith(account: "", password: "", checkpassword: "")
+
+    }
+    
+    func updateTextFiledWith(account: String? = nil, password: String? = nil, checkpassword: String? = nil) {
+        
+        if let accountValue = account {
+            accountTextLabel.text = ""
+        }
+        if let passwordValue = password {
+            passwordTextLabel.text = ""
+        }
+        if let checkpassordValue = checkpassword {
+            checkTexTLabel.text = ""
         }
     }
     
+    
+    func showPopAlert(title: String, message: String, style: UIAlertController.Style = .alert) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: dismissAlert)        // let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func dismissAlert(sender: UIAlertAction) -> Void {
+        updateTextFiledWith(account: nil, password: nil, checkpassword: nil)
+    } //影片這邊打balance: 0 要改掉，算是留給我們的測試。
+
 
 }
+
 
